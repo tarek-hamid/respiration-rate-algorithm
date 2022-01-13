a4e99e82cd372d001e8923fae749dd5077fe084d
DESKTOP-6V8LD4B\tarek <hamidtarek3@gmail.com>
2022-01-13 15:05:10 -0500

    Updated algo to include resp rate graph

--- respiration_rate_algorithm.m
+++ respiration_rate_algorithm.m
@@ -4,15 +4,15 @@
 % Initialize data
 clear;
 clc;
-load("dataset_1.mat");
+load("dataset_2.mat");
 
 %% Plot sample of data by extracting one column
 
 % Extract first column
-sample_data = dataset_1(:,1);
+sample_data = dataset_2(:,4);
 
-% Look at first 1000 samples
-sample_data = sample_data(1:1000);
+% Look at sample data
+sample_data = sample_data(2000:3000);
 
 % Calculate time axis w/ sampling freq 17hz
 fs = 17;
@@ -45,57 +45,106 @@ title('(Normalized) Frequency Analysis of Sample Data')
 xlabel('Frequency')
 ylabel('Magnitude')
 
-%% Pre-processing - low pass filtering 
+%% Pre-processing - low pass filtering / moving avg filter
 
-% 10th order low pass Butterworth filter, 0.1 normalized freq cutoff
-[b, a] = butter(10, 0.1, 'low');
+% frequency domain - low pass Butterworth filter, 0.1 normalized freq cutoff
+[b, a] = butter(5, 0.1, 'low');
 filtered_sample_data = filter(b, a, sample_data);
+
+% time domain - Moving avg filter
+moving_avg_sample_data = movmean(sample_data, 10);
+moving_avg_sample_data = movmean(moving_avg_sample_data, 10);
+moving_avg_sample_data = movmean(moving_avg_sample_data, 5);
+
+% Plot
 figure(3);
-subplot(2,1,1)
-plot(t, sample_data);
+subplot(3,1,1)
+plot(t(50:end), sample_data(50:end));
 title('Raw Respiration Signal')
 xlabel('Time (s)')
 ylabel('Magnitude')
-subplot(2,1,2)
-plot(t, filtered_sample_data);
-title('Filtered Respiration Signal')
+subplot(3,1,2)
+plot(t(50:end), filtered_sample_data(50:end));
+title('Low Pass Filtered Respiration Signal')
+xlabel('Time (s)')
+ylabel('Magnitude')
+subplot(3,1,3)
+plot(t(50:end), moving_avg_sample_data(50:end));
+title('Moving Avg Respiration Signal')
 xlabel('Time (s)')
 ylabel('Magnitude')
 
-%% Algorithm 1 - simple peak detection
+%% Algorithm 1 - peak detection with low pass filter
 
+% Could also use find peaks fnc from MATLAB
 % Can use mean of signal as baseline to decrease sensitivity
+% Peak = sample greater than two nearest neighbors
 
 % Initialize breath count in signal
-breath_count = 0;
+low_pass_breath_count = 0;
+
+% initialize respiration rates array
+low_pass_filter_respiration_rates = [];
+
+% initialize point where last breath was detected
+previous_breath_timestamp = 0;
+time_since_last_breath = 0;
 
 % Iterate through signal. Compare neighbors and check if both less than
-% current point. If yes -> add to breath count. 
-% Peak = sample greater than two nearest neighbors
+% current point. If yes -> add to breath count and add to respiration rate
+% array
 for i = 2 : length(filtered_sample_data) - 1
     if(filtered_sample_data(i) > filtered_sample_data(i - 1) && filtered_sample_data(i) > filtered_sample_data(i + 1))
-        breath_count = breath_count + 1;
+        low_pass_breath_count = low_pass_breath_count + 1;
+        time_since_last_breath = t(i) - previous_breath_timestamp;
+        previous_breath_timestamp = t(i);
     end
+    low_pass_filter_respiration_rates(end+1) = 60 / time_since_last_breath;
 end
 
-% Calculate respiration rate
-duration_in_minutes = N / fs / 60;
-peak_detection_respiration_rate = floor(breath_count / duration_in_minutes);
-display(peak_detection_respiration_rate);
+%% Algorithm 2 - peak detection with moving average filter
 
-%% Algorithm 2 - moving average filter 
+% Initialize breath count in signal
+moving_avg_breath_count = 0;
 
-% Use moving average filter to smooth data and ignore small transients.
-moving_avg = movmean(filtered_sample_data, 7);
+% initialize respiration rates array
+moving_avg_respiration_rates = [];
+
+% initalize point where last breath was detected
+previous_breath_timestamp = 0;
+time_since_last_breath = 0;
 
 % Traverse moving avg vector (same methodology as Algorithm 1).
 breath_count = 0;
-for i = 2 : length(moving_avg) - 1
-    if(moving_avg(i) > moving_avg(i - 1) && moving_avg(i) > moving_avg(i + 1))
-        breath_count = breath_count + 1;
+for i = 2 : length(moving_avg_sample_data) - 1
+    if(moving_avg_sample_data(i) > moving_avg_sample_data(i - 1) && moving_avg_sample_data(i) > moving_avg_sample_data(i + 1))
+        moving_avg_breath_count = moving_avg_breath_count + 1;
+        time_since_last_breath = t(i) - previous_breath_timestamp;
+        previous_breath_timestamp = t(i);
     end
+    moving_avg_respiration_rates(end+1) = 60 / time_since_last_breath;
 end
 
-% Calculate respiration rate
-moving_avg_respiration_rate = floor(breath_count / duration_in_minutes);
-display(moving_avg_respiration_rate);
+%% Plot respiration rates and calculate mean respiration rates
+
+% Calculate mean low pass filtered respiration rate
+duration_in_minutes = N / fs / 60;
+mean_low_pass_respiration_rate = floor(low_pass_breath_count / duration_in_minutes);
+display(mean_low_pass_respiration_rate);
+
+% Calculate mean moving average respiration rate
+mean_moving_avg_respiration_rate = floor(moving_avg_breath_count / duration_in_minutes);
+display(mean_moving_avg_respiration_rate); 
+
+% Plot respiration rates
+figure(4)
+subplot(2,1,1)
+plot((linspace(0,t(end),length(low_pass_filter_respiration_rates))),low_pass_filter_respiration_rates)
+title('Low Pass Filter Rates')
+xlabel('Time (s)')
+ylabel('Rate (breaths/min)')
+subplot(2,1,2)
+plot((linspace(0,t(end),length(moving_avg_respiration_rates))), moving_avg_respiration_rates)
+title('Moving Avg Filter Rates')
+xlabel('Time (s)')
+ylabel('Rate (breaths/min)')
